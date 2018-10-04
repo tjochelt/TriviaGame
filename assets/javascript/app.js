9bf82f3319dbbd774338cdb38d0abe181a7764e3
Tom Ocheltree <tom.ocheltree@gmail.com>
2018-10-03 20:28:25 -0500

    js work

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,44 +1,125 @@
 $(document).ready(function() {
-//object with arrays of questions
-var trivia = {
-questionOne: ["This is question one", "this is choice 1", "this is choice 2", "this is choice 3",  "this is choice 4"],
-questionTwo: ["This is question one", "this is choice 1", "this is choice 2", "this is choice 3",  "this is choice 4"],
-questionThree: ["This is question one", "this is choice 1", "this is choice 2", "this is choice 3",  "this is choice 4"],
-questionFour: ["This is question one", "this is choice 1", "this is choice 2", "this is choice 3",  "this is choice 4"],
-}
-//Start button to display first question
+  //array with object of questions
+  $(".question-bucket").hide();
 
-// function startQuiz() {
-//     var x = $(".question-bucket");
-//     if (x.style.display === "none") {
-//         x.style.display = "block";
-//     } else {
-//         x.style.display = "none";
-//     }
-// }
-$("#start-button").click(startQuiz(){
-    console.log("hi tom");
+  var trivia = [
+    {
+      question: "This is question one",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "this is choice 1"
+    },
+    {
+      question: "This is question two",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "this is choice 3"
+    },
+    {
+      question: "This is question three",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    },
+    {
+      question: "This is question four",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    },
+    {
+      question: "This is question five",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    },
+    {
+      question: "This is question six",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    },
+    {
+      question: "This is question seven",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    },
+    {
+      question: "This is question eight",
+      answers: [
+        "this is choice 1",
+        "this is choice 2",
+        "this is choice 3",
+        "this is choice 4"
+      ],
+      correct: "This is question 3"
+    }
+  ];
+  //Start button to display first question
+
+  function startQuiz() {
+    // var x = $("#question");
+    cycleQuestion();
     $(".question-bucket").show();
     $("#start-button").hide();
-};
-// $("#hide").click(function(){
-//     $("p").hide();
-// });
+  }
+  function cycleQuestion() {
+    setTimeout(thirtySeconds, 1000 * 3);
+  }
 
-//Display question 1 with four choices. Starts timer. 
-//Set timer for 20 seconds. 
+  $("#start-button").click(function() {
+    console.log("hi tom");
+    startQuiz();
+  });
 
-//if timer runs out, say time's up and display answer. add one to unanswered total. 
+  //boolean to tell if you're on the question or not.
 
-//Start shorter timer whilst showing correct answer. move on to next question when shorter timer runs out. 
+  //Display question 1 with four choices. Starts timer.
+  //Set timer for 30 seconds.
+  function thirtySeconds() {
+    console.log("tom o");
+  }
+  //append the answers and next question to the UI
+  //use for loop
 
-//if if user guesses wrong, say you're wrong and display answer. add one to incorrect total.
+  //if timer runs out, say times up and display answer. add one to unanswered total.
 
-//if user guesses correct answer, say you're right. add one to correct total. 
+  //Start shorter timer whilst showing correct answer. move on to next question when shorter timer runs out.
 
-//when done with list of questions, show finished screen, total score. 
+  //if if user guesses wrong, say youre wrong and display answer. add one to incorrect total.
 
-//start over button restarts game
+  //if user guesses correct answer, say youre right. add one to correct total.
 
+  //when done with list of questions, show finished screen, total score.
 
-}
+  //start over button restarts game
+});
